e04a0092b3817b04b0e235ebd6f3c021ca424ac8
HuangYiHui <33647653+HuangYiHui@users.noreply.github.com>
2017-12-17 09:58:41 +0800

    完成BS_GY30_Demo

---
+++ arduino/demo/BS_GY30_Demo/BS_GY30_Demo/BS_GY30_Demo.ino
@@ -0,0 +1,29 @@
+
+
+#include <bh1750xtra.h>
+#include <Wire.h>
+
+BH1750xtra bhSensor;
+
+void setup() {
+    uint8_t error;
+    Serial.begin(9600);
+    Wire.begin(); //初始化I2C总线
+    Wire.beginTransmission(35);
+    //取消测试每个方法
+    bhSensor.Set_To_Max_Resolution_Measurement(); 
+    delay(1000);
+}
+
+void loop() {
+    int8_t nBytesRead = bhSensor.Read_I2C_To_Buffer();
+    if (nBytesRead != 2)  
+		Serial.print("I2C reading error "); 
+	else {
+		int value = bhSensor.Get_Read_Lux();
+        Serial.print(value); 
+		Serial.println(" lx ");
+    }
+    delay(1000);
+}
+
---
+++ arduino/demo/BS_GY30_Demo/BS_GY30_Demo/Debug/BS_GY30_Demo.cpp.d
@@ -0,0 +1,16 @@
+D:\software\Temp\VMBuilds\BS_GY30_Demo\uno\Debug\BS_GY30_Demo.cpp.o: \
+ D:\software\Temp\VMBuilds\BS_GY30_Demo\uno\Debug\BS_GY30_Demo.cpp \
+ D:\software1\arduino-1.8.5\hardware\arduino\avr\cores\arduino/Arduino.h \
+ D:\software1\arduino-1.8.5\hardware\arduino\avr\cores\arduino/binary.h \
+ D:\software1\arduino-1.8.5\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ D:\software1\arduino-1.8.5\hardware\arduino\avr\cores\arduino/WString.h \
+ D:\software1\arduino-1.8.5\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ D:\software1\arduino-1.8.5\hardware\arduino\avr\cores\arduino/Stream.h \
+ D:\software1\arduino-1.8.5\hardware\arduino\avr\cores\arduino/Print.h \
+ D:\software1\arduino-1.8.5\hardware\arduino\avr\cores\arduino/Printable.h \
+ D:\software1\arduino-1.8.5\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ D:\software1\arduino-1.8.5\hardware\arduino\avr\cores\arduino/Arduino.h \
+ D:\software1\arduino-1.8.5\hardware\arduino\avr\variants\standard/pins_arduino.h \
+ D:\software1\arduino-1.8.5\libraries\BH1750xtra/bh1750xtra.h \
+ D:\software1\arduino-1.8.5\hardware\arduino\avr\libraries\Wire\src/Wire.h \
+ D:\software1\arduino-1.8.5\hardware\arduino\avr\cores\arduino/Stream.h
